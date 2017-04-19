docbleach/DocBleach

commit 271f1e605a4f757f142d0596d80e98b36c85ebe7

:bug: OOXML: Use deletePart instead of removePart
Using the `delete` method instead of the `remove` one, we let Apache
POI take care of the relationships.

module/module-office/src/main/java/xyz/docbleach/modules/ooxml/OOXMLBleach.java
@@ -116,8 +116,7 @@ public class OOXMLBleach implements IBleach {
         // cf https://msdn.microsoft.com/fr-fr/library/aa338205(v=office.12).aspx
         if (isForbiddenType(part.getContentTypeDetails())) {
             LOGGER.debug(SUSPICIOUS_OOXML_FORMAT, contentType, part.getPartName(), part.getSize());
-            // pkg.clearRelationships();
-            pkg.removePart(part.getPartName());
+            pkg.deletePart(part.getPartName());
             session.recordThreat("Dynamic content", SEVERITY.HIGH);
         }
     }

commit 1df88a92a3919f1f53e8f53790b6faddf6181972

:bug: OOXML: Fix regression introduced in 271f1e60
If a part exists without a linked relation, the deletePart method
throws an exception: `throw new IllegalArgumentException("partName");`

This is not the expected behaviour here: malformed files, or partly
sanitized files match this pattern.

Instead, we check if the relationship exists and is in the package.

module/module-office/src/main/java/xyz/docbleach/modules/ooxml/OOXMLBleach.java
@@ -176,8 +176,25 @@ public class OOXMLBleach implements IBleach {
         ContentType type = part.getContentTypeDetails();
         if (isForbiddenType(type) || isStrangeContentType(type)) {
             LOGGER.debug(SUSPICIOUS_OOXML_FORMAT, contentType, part.getPartName(), part.getSize());
-            pkg.deletePart(part.getPartName());
+            deletePart(pkg, part.getPartName());
             session.recordThreat("Dynamic content", SEVERITY.HIGH);
+        }
+    }
+
+    /**
+     * Delete the part with the specified name and its associated relationships part if one exists.
+     * <p>
+     * Unlike {@link OPCPackage#deletePart(PackagePartName)}, this checks if the relationship exists
+     * before trying to remove it, instead of throwing an exception.
+     *
+     * @param partName Name of the part to delete
+     */
+    private void deletePart(OPCPackage pkg, PackagePartName partName) {
+        pkg.removePart(partName);
+
+        PackagePartName relationshipPartName = PackagingURIHelper.getRelationshipPartName(partName);
+        if (relationshipPartName != null && pkg.containPart(relationshipPartName)) {
+            pkg.removePart(relationshipPartName);
         }
     }
 