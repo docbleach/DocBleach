a6c2bdff1194645efc8d8e6dcd01d24ef8b207e1
PunKeel <punkeel@me.com>
2017-04-20 01:46:01 +0200

    Revert ":art: OOXML: Declare modifiers in the right order"
    This reverts commit f05f4bd14dfb52d3dd4d29f4a47a467e6de8d837.

--- module/module-office/src/main/java/xyz/docbleach/modules/ooxml/ContentTypes.java
+++ module/module-office/src/main/java/xyz/docbleach/modules/ooxml/ContentTypes.java
@@ -3,42 +3,42 @@ package xyz.docbleach.modules.ooxml;
 @SuppressWarnings("ALL")
 public final class ContentTypes {
     // Main Parts (PowerPoint)
-    private static final String MAIN_PPTX = "application/vnd.openxmlformats-officedocument.presentationml.presentation.main+xml";
-    private static final String MAIN_PPTM = "application/vnd.ms-powerpoint.presentation.macroEnabled.main+xml";
-    private static final String MAIN_POTX = "application/vnd.openxmlformats-officedocument.presentationml.template.main+xml";
-    private static final String MAIN_POTM = "application/vnd.ms-powerpoint.template.macroEnabled.main+xml";
-    private static final String MAIN_PPSX = "application/vnd.openxmlformats-officedocument.presentationml.slideshow.main+xml";
-    private static final String MAIN_PPSM = "application/vnd.ms-powerpoint.slideshow.macroEnabled.main+xml";
-    private static final String MAIN_PPAM = "application/vnd.ms-powerpoint.addin.macroEnabled.main+xml";
+    public final static String MAIN_PPTX = "application/vnd.openxmlformats-officedocument.presentationml.presentation.main+xml";
+    public final static String MAIN_PPTM = "application/vnd.ms-powerpoint.presentation.macroEnabled.main+xml";
+    public final static String MAIN_POTX = "application/vnd.openxmlformats-officedocument.presentationml.template.main+xml";
+    public final static String MAIN_POTM = "application/vnd.ms-powerpoint.template.macroEnabled.main+xml";
+    public final static String MAIN_PPSX = "application/vnd.openxmlformats-officedocument.presentationml.slideshow.main+xml";
+    public final static String MAIN_PPSM = "application/vnd.ms-powerpoint.slideshow.macroEnabled.main+xml";
+    public final static String MAIN_PPAM = "application/vnd.ms-powerpoint.addin.macroEnabled.main+xml";
 
     // Main Parts (Word)
-    private static final String MAIN_DOCX = "application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml";
-    private static final String MAIN_DOCM = "application/vnd.ms-word.document.macroEnabled.main+xml";
-    private static final String MAIN_DOTX = "application/vnd.openxmlformats-officedocument.wordprocessingml.template.main+xml";
-    private static final String MAIN_DOTM = "application/vnd.ms-word.template.macroEnabledTemplate.main+xml";
+    public final static String MAIN_DOCX = "application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml";
+    public final static String MAIN_DOCM = "application/vnd.ms-word.document.macroEnabled.main+xml";
+    public final static String MAIN_DOTX = "application/vnd.openxmlformats-officedocument.wordprocessingml.template.main+xml";
+    public final static String MAIN_DOTM = "application/vnd.ms-word.template.macroEnabledTemplate.main+xml";
 
     // Main Parts (Excel)
-    private static final String MAIN_XLSB = "application/vnd.ms-excel.sheet.binary.macroEnabled.main";
-    private static final String MAIN_XLSX = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml";
-    private static final String MAIN_XLSM = "application/vnd.ms-excel.sheet.macroEnabled.main+xml";
-    private static final String MAIN_XLTX = "application/vnd.openxmlformats-officedocument.spreadsheetml.template.main+xml";
-    private static final String MAIN_XLTM = "application/vnd.ms-excel.template.macroEnabled.main+xml";
-    private static final String MAIN_XLAM = "application/vnd.ms-excel.addin.macroEnabled.main+xml";
+    public final static String MAIN_XLSB = "application/vnd.ms-excel.sheet.binary.macroEnabled.main";
+    public final static String MAIN_XLSX = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml";
+    public final static String MAIN_XLSM = "application/vnd.ms-excel.sheet.macroEnabled.main+xml";
+    public final static String MAIN_XLTX = "application/vnd.openxmlformats-officedocument.spreadsheetml.template.main+xml";
+    public final static String MAIN_XLTM = "application/vnd.ms-excel.template.macroEnabled.main+xml";
+    public final static String MAIN_XLAM = "application/vnd.ms-excel.addin.macroEnabled.main+xml";
 
     // VBA related
-    private static final String VBA_DATA = "application/vnd.ms-word.vbaData+xml";
-    private static final String VBA_PROJECT = "application/vnd.ms-office.vbaProject";
-    private static final String VBA_PROJECT_SIGNATURE = "application/vnd.ms-office.vbaProjectSignature";
+    public final static String VBA_DATA = "application/vnd.ms-word.vbaData+xml";
+    public final static String VBA_PROJECT = "application/vnd.ms-office.vbaProject";
+    public final static String VBA_PROJECT_SIGNATURE = "application/vnd.ms-office.vbaProjectSignature";
 
     // PostScript
-    private static final String POSTSCRIPT = "application/postscript";
+    public final static String POSTSCRIPT = "application/postscript";
 
     // OLE Objects
-    private static final String OLE_OBJECT = "application/vnd.openxmlformats-officedocument.oleObject";
-    private static final String PACKAGE = "application/vnd.openxmlformats-officedocument.package";
+    public final static String OLE_OBJECT = "application/vnd.openxmlformats-officedocument.oleObject";
+    public final static String PACKAGE = "application/vnd.openxmlformats-officedocument.package";
 
     // ActiveX Controls
-    private static final String ACTIVEX = "application/vnd.ms-office.activeX";
-    private static final String OPENXML_ACTIVEX = "application/vnd.openxmlformats-officedocument.activeX";
-    private static final String OPENXML_ACTIVEX_XML = "application/vnd.openxmlformats-officedocument.activeX+xml";
+    public final static String ACTIVEX = "application/vnd.ms-office.activeX";
+    public final static String OPENXML_ACTIVEX = "application/vnd.openxmlformats-officedocument.activeX";
+    public final static String OPENXML_ACTIVEX_XML = "application/vnd.openxmlformats-officedocument.activeX+xml";
 }
--- module/module-office/src/main/java/xyz/docbleach/modules/ooxml/Relations.java
+++ module/module-office/src/main/java/xyz/docbleach/modules/ooxml/Relations.java
@@ -2,23 +2,23 @@ package xyz.docbleach.modules.ooxml;
 
 @SuppressWarnings("ALL")
 public final class Relations {
-    private static final String HYPERLINK = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/hyperlink";
+    public final static String HYPERLINK = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/hyperlink";
 
     // Macro
-    private static final String VBA_PROJECT = "http://schemas.microsoft.com/office/2006/relationships/vbaProject";
-    private static final String VBA_PROJECT_SIGNATURE = "http://schemas.microsoft.com/office/2006/relationships/vbaProjectSignature";
-    private static final String WORD_VBA_DATA = "http://schemas.microsoft.com/office/2006/relationships/wordVbaData";
+    public final static String VBA_PROJECT = "http://schemas.microsoft.com/office/2006/relationships/vbaProject";
+    public final static String VBA_PROJECT_SIGNATURE = "http://schemas.microsoft.com/office/2006/relationships/vbaProjectSignature";
+    public final static String WORD_VBA_DATA = "http://schemas.microsoft.com/office/2006/relationships/wordVbaData";
 
     // OLE Objects
-    private static final String OPENXML_OLE_OBJECT = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/oleObject";
-    private static final String OLE_OBJECT = "http://schemas.microsoft.com/office/2006/relationships/oleObject";
-    private static final String E2_OBJECT = "http://schemas.microsoft.com/office/2006/relationships/e2Object";
-    private static final String E1_OBJECT = "http://schemas.microsoft.com/office/2006/relationships/e1Object";
+    public final static String OPENXML_OLE_OBJECT = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/oleObject";
+    public final static String OLE_OBJECT = "http://schemas.microsoft.com/office/2006/relationships/oleObject";
+    public final static String E2_OBJECT = "http://schemas.microsoft.com/office/2006/relationships/e2Object";
+    public final static String E1_OBJECT = "http://schemas.microsoft.com/office/2006/relationships/e1Object";
 
     // ActiveX Controls
-    private static final String OPENXML_CONTROL = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/control";
-    private static final String OPENXML_ACTIVEX_CONTROL = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/activeXControl";
-    private static final String OPENXML_ACTIVEX_CONTROL_BIN = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/activeXControlBinary";
-    private static final String ACTIVEX_CONTROL = "http://schemas.microsoft.com/office/2006/relationships/activeXControl";
-    private static final String ACTIVEX_CONTROL_BIN = "http://schemas.microsoft.com/office/2006/relationships/activeXControlBinary";
+    public final static String OPENXML_CONTROL = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/control";
+    public final static String OPENXML_ACTIVEX_CONTROL = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/activeXControl";
+    public final static String OPENXML_ACTIVEX_CONTROL_BIN = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/activeXControlBinary";
+    public final static String ACTIVEX_CONTROL = "http://schemas.microsoft.com/office/2006/relationships/activeXControl";
+    public final static String ACTIVEX_CONTROL_BIN = "http://schemas.microsoft.com/office/2006/relationships/activeXControlBinary";
 }
