8d965af188473fc7c8620ebffd0b4ce4a0df9178
PunKeel <punkeel@me.com>
2017-04-24 01:20:35 +0200

    :green_heart: Add Coverity Scan for tags

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+set -e
 
-mvn deploy --settings scripts/deploy_settings.xml -DperformRelease=true -DskipTests=true
-exit $?
+coverity_scan() {
+  if [ -n "${TRAVIS_TAG}" ] || [ "${TRAVIS_PULL_REQUEST}" = "true" ]
+  then
+    echo "Skipping coverity scan."
+    return
+  fi
+  export COVERITY_SCAN_PROJECT_NAME="${TRAVIS_REPO_SLUG}"
+  export COVERITY_SCAN_NOTIFICATION_EMAIL="punkeel@me.com"
+  export COVERITY_SCAN_BUILD_COMMAND="mvn package"
+  export COVERITY_SCAN_BUILD_COMMAND_PREPEND="mvn clean"
+  export COVERITY_SCAN_BRANCH_PATTERN="master"
+  curl -s "https://scan.coverity.com/scripts/travisci_build_coverity_scan.sh" | bash || :
+}
+
+mvn_deploy() {
+    mvn deploy --settings scripts/deploy_settings.xml -DperformRelease=true -DskipTests=true
+}
+
+mvn_deploy
+coverity_scan
