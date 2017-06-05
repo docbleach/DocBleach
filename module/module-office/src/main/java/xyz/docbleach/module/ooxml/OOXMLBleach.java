d7069a3ba2c3327f6da19a22adaf4fd92ec0241b
Sébastien Mériot <sebastien.meriot@corp.ovh.com>
2017-06-05 21:35:06 +0200

    fix(ooxml): MS Office crashes when relationship cannot be found, so give him a dummy file to load.

--- module/module-office/src/main/java/xyz/docbleach/module/ooxml/OOXMLBleach.java
+++ module/module-office/src/main/java/xyz/docbleach/module/ooxml/OOXMLBleach.java
@@ -1,12 +1,30 @@
 package xyz.docbleach.module.ooxml;
 
+import static xyz.docbleach.api.threat.ThreatBuilder.threat;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 import org.apache.poi.UnsupportedFileFormatException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.openxml4j.opc.*;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.openxml4j.opc.PackagePart;
+import org.apache.poi.openxml4j.opc.PackagePartName;
+import org.apache.poi.openxml4j.opc.PackageRelationship;
+import org.apache.poi.openxml4j.opc.PackagingURIHelper;
+import org.apache.poi.openxml4j.opc.RelationshipSource;
+import org.apache.poi.openxml4j.opc.TargetMode;
 import org.apache.poi.openxml4j.opc.internal.ContentType;
 import org.apache.poi.poifs.filesystem.DocumentFactoryHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import xyz.docbleach.api.BleachSession;
 import xyz.docbleach.api.bleach.Bleach;
 import xyz.docbleach.api.exception.BleachException;
@@ -15,13 +33,6 @@ import xyz.docbleach.api.threat.ThreatAction;
 import xyz.docbleach.api.threat.ThreatSeverity;
 import xyz.docbleach.api.threat.ThreatType;
 import xyz.docbleach.api.util.StreamUtils;
-
-import java.io.*;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-
-import static xyz.docbleach.api.threat.ThreatBuilder.threat;
 
 /**
  * Sanitizes Office 2007+ documents (OOXML), in place. This format has two interesting things for
@@ -35,6 +46,10 @@ public class OOXMLBleach implements Bleach {
     private static final String EXTERNAL_RELATION_FORMAT = "Found an external relationship from '{}' to '{}' (type '{}')";
     private static final String OOXWORD_SCHEME = "ooxWord";
 
+    private static final String DUMMY_FILE_PART_NAME = "/bleach/bleach";
+    private static final String DUMMY_FILE_CONTENT = "BLEACHED";
+    private static final PackagePartName DUMMY_PACKAGE_PART_NAME = createPartName(DUMMY_FILE_PART_NAME);
+
     private static final String[] WHITELISTED_RELATIONS = new String[]{
             // Hyperlinks should be safe enough, right?
             Relations.HYPERLINK
@@ -45,6 +60,12 @@ public class OOXMLBleach implements Bleach {
             Relations.VBA_PROJECT,
             Relations.VBA_PROJECT_SIGNATURE,
             Relations.WORD_VBA_DATA,
+
+            // OLE Objects
+            Relations.OPENXML_OLE_OBJECT,
+            Relations.OLE_OBJECT,
+            Relations.E1_OBJECT,
+            Relations.E2_OBJECT,
 
             // ActiveX Controls
             Relations.OPENXML_CONTROL,
@@ -62,6 +83,10 @@ public class OOXMLBleach implements Bleach {
 
             // Blacklisting Postscript to prevent 0days
             ContentTypes.POSTSCRIPT,
+
+            // OLE Objects
+            ContentTypes.OLE_OBJECT,
+            ContentTypes.PACKAGE,
 
             // ActiveX objects
             ContentTypes.ACTIVEX,
@@ -170,9 +195,31 @@ public class OOXMLBleach implements Bleach {
 
             remapContentType(session, part);
         }
+
+        // If threats have been removed, then add the dummy file so the relationship
+        // still refers to an existing dummy object.
+        if (session.threatCount() > 0) {
+        	pushDummyFile(pkg);
+        }
     }
 
-    void remapContentType(BleachSession session, PackagePart part) throws InvalidFormatException {
+    /**
+     * The dummy file tries to prevent Microsoft Office from crashing because they forgot a lot of "!= null"
+     * while checking if a resource is valid.
+     * @param pkg Document to put the file in
+     * @throws InvalidFormatException
+     */
+    private void pushDummyFile(OPCPackage pkg) throws InvalidFormatException {
+        
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
+        	bos.write(DUMMY_FILE_CONTENT.getBytes());
+        	pkg.createPart(PackagingURIHelper.createPartName(DUMMY_FILE_PART_NAME), ContentTypes.TEXT_PLAIN, bos);
+        } catch(IOException ex) {
+        	LOGGER.error("Error occured while pushing the file in the document. But, it's not that critical so I'm gonna continue.", ex);
+        }
+	}
+
+	void remapContentType(BleachSession session, PackagePart part) throws InvalidFormatException {
         String oldContentType = part.getContentType();
         if (!REMAPPED_CONTENT_TYPES.containsKey(oldContentType)) {
             return;
@@ -215,8 +262,8 @@ public class OOXMLBleach implements Bleach {
         }
 
         if (isBlacklistedRelationType(relationshipType)) {
-            pkg.removeRelationship(relationship.getId());
-
+        	replaceRelationship(pkg, relationship);
+        
             Threat threat = threat()
                     .type(ThreatType.ACTIVE_CONTENT)
                     .severity(ThreatSeverity.HIGH)
@@ -230,9 +277,9 @@ public class OOXMLBleach implements Bleach {
         }
 
         if (TargetMode.EXTERNAL.equals(relationship.getTargetMode())) {
-            pkg.removeRelationship(relationship.getId());
+            replaceRelationship(pkg, relationship);
+            
             ThreatSeverity severity = ThreatSeverity.HIGH;
-
             if (OOXWORD_SCHEME.equals(relationship.getTargetURI().getScheme())) {
                 // Fake external relationship
                 severity = ThreatSeverity.EXTREME;
@@ -258,7 +305,21 @@ public class OOXMLBleach implements Bleach {
         }
     }
 
-    private boolean isBlacklistedRelationType(String relationshipType) {
+    /**
+     * Replace a relationship by a fake on using the {@link #pushDummyFile(OPCPackage) dummy file} so MS Office does not crash
+     * because the relationship does not exist.
+     * @param pkg Where the relationship must be removed from
+     * @param relationship The relationship to replace
+     * @see #pushDummyFile
+     */
+    private void replaceRelationship(RelationshipSource pkg, PackageRelationship relationship) {
+    	String rId = relationship.getId();
+    	
+    	pkg.removeRelationship(rId);
+    	pkg.addRelationship(DUMMY_PACKAGE_PART_NAME, TargetMode.INTERNAL, Relations.OPENXML_OLE_OBJECT, rId);
+	}
+
+	private boolean isBlacklistedRelationType(String relationshipType) {
         for (String rel : BLACKLISTED_RELATIONS) {
             if (rel.equalsIgnoreCase(relationshipType)) {
                 return true;
@@ -343,5 +404,17 @@ public class OOXMLBleach implements Bleach {
                 LOGGER.error("Unknown content type: {}", contentType);
                 return true;
         }
+    }
+    
+    private static PackagePartName createPartName(final String name) {
+    	PackagePartName res = null;
+    	
+    	try {
+    		res = PackagingURIHelper.createPartName(name);
+    	} catch(InvalidFormatException ex) {
+    		LOGGER.error("Cannot initialize the PackagePartName: ", ex);
+    	}
+    	
+    	return res;
     }
 }
