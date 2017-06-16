17b0aa6c41e8ea6e9b9d11cc81200bf92f6a09c3
PunKeel <punkeel@me.com>
2017-06-16 15:55:02 +0200

    Travis: Build for amd64 and not x86

--- scripts/jet-build.sh
+++ scripts/jet-build.sh
@@ -10,8 +10,8 @@ if [ -z "${JET_URL}" ]
     return
 fi
 
-wget -q --no-check-certificate -O jet-1130-pro-en-linux-x86.bin "${JET_URL}"
-chmod +x ./jet-1130-pro-en-linux-x86.bin
-./jet-1130-pro-en-linux-x86.bin -batch -no-aftrun
-export JET_HOME="`pwd`/jet11.3-pro-x86/"
-mvn jet:testrun jet:build
+wget -q --no-check-certificate -O jet-1130-pro-en-linux-amd64.bin "${JET_URL}"
+chmod +x ./jet-1130-pro-en-linux-amd64.bin
+./jet-1130-pro-en-linux-amd64.bin -batch -no-aftrun
+export JET_HOME="`pwd`/jet11.3-pro-amd64/"
+mvn jet:testrun jet:build -pl cli --batch-mode -DskipTest 
